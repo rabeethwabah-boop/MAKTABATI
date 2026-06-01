06a2457d3b10e224a8afe660b0e3363492c7891d
Rabee <rabeethwabah@gmail.com>
2026-06-01 21:27:21 +0300

    Simplify health check

--- artifacts/api-server/src/routes/health.ts
+++ artifacts/api-server/src/routes/health.ts
@@ -1,11 +1,10 @@
-import { Router, type IRouter } from "express";
-import { HealthCheckResponse } from "@workspace/api-zod";
+import { Router } from "express";
 
-const router: IRouter = Router();
+const router = Router();
 
-router.get("/healthz", (_req, res) => {
-  const data = HealthCheckResponse.parse({ status: "ok" });
-  res.json(data);
+// @ts-ignore
+router.get("/healthz", (req, res) => {
+  res.json({ status: "ok" });
 });
 
 export default router;
