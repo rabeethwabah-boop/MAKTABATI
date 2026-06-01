1e6061ea8b2090b21b6015f68c5c14800a4a0efa
Rabee <rabeethwabah@gmail.com>
2026-06-01 20:56:12 +0300

    fixing TS errors

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,30 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttp, { type Options as PinoHttpOptions, type SerializedRequest, type SerializedResponse } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+const pinoOptions: PinoHttpOptions = {
+  logger,
+  serializers: {
+    req(req: Request): SerializedRequest {
+      return {
+        id: (req as any).id, // id may not exist on Express.Request by default
+        method: req.method,
+        url: req.url?.split("?")[0],
+      };
     },
-  }),
-);
+    res(res: Response): SerializedResponse {
+      return {
+        statusCode: res.statusCode,
+      };
+    },
+  },
+};
+
+app.use(pinoHttp(pinoOptions));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
